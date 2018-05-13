4d6c3e085cc6578b2f0ac0a28190a921b20ed9a6
lrosic00 <lrosic00@fesb.hr>
2018-05-13 17:58:42 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ssglu/androidblagajna/ArticleClass.java
@@ -0,0 +1,13 @@
+package com.example.ssglu.androidblagajna;
+
+
+public class ArticleClass {
+
+    int id;
+    String name;
+    Double price;
+    int quantity;
+    String category;
+    boolean isVisible = true;
+
+}
